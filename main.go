6dae83e9929989289c6ec62762c5e9735039cc11
Lxy417165709 <417165709@qq.com>
2021-06-25 00:20:52 +0800

    .

--- main.go
+++ main.go
@@ -1,25 +1,65 @@
-package lg
+package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"time"
 
-)
+// ------------------------ MyMap ------------------------
+type MyMap struct{
+	Cap int
+	Nodes []*Node
+	mutex Mutex
+}
+
+func NewMap(cap int) *MyMap{
+	return &MyMap{
+		Cap:cap,
+		Nodes : make([]*Node,cap)
+	}
+}
+
+func (m *MyMap) RangeAll() []int{
+	m.mutex.Lock()
+	defer m.mutex.UnLock()
+
+	keys := make([]int,0)
+	for _, nodeHead := range m.Nodes {
+		curHead := nodeHead
+		for curHead != nil{
+			keys = append(keys,curHead.Key)
+			curHead = curHead.Next
+		}
+	}
+	sort.Ints(keys)
+	return keys
+}
+
+
+func (m *MyMap) Insert(key,value int) {
+	m.mutex.Lock()
+	defer m.mutex.UnLock()
+
+	insertIndex := key%m.Cap
+	head := m.Nodes[insertIndex]
+	insertNdoe :=  &Node{
+		Key:key,
+		Value:value,
+	}
+	if head == nil{
+		m.Nodes[insertIndex] = insertNdoe
+		return
+	}
+	for head.Next!=nil{
+		head = head.Next
+	}
+	head.Next = insertNdoe
+}
+
+
+// ------------------------ 底层结构 ------------------------
+type Node struct{
+	Key int
+	Value interface{}
+	Next *Node
+}
 
 func main() {
-	board := NewLiveGameBoard(22, 150,15)
-	for {
-		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
-		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
 
-		board.Show()
-		board.NextState()
-		time.Sleep(100 * time.Millisecond)
-	}
 }
